213e0279fa29d77804b495846d56086fabf8d049
A601-13 <tnk.dat.class@gmail.com>
2022-06-16 13:36:05 +0900

    復習

--- fukuvHensu/Form1.cs
+++ fukuvHensu/Form1.cs
@@ -17,5 +17,35 @@ namespace fukuvHensu
             InitializeComponent();
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top += -5;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left += -5;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left += 5;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top += 5;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("玉井秀兵");
+        }
     }
 }
