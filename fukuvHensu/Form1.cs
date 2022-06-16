f43662edc6c5fb65c2e6e9355943178d207f3674
A601-13 <tnk.dat.class@gmail.com>
2022-06-16 14:13:51 +0900

    追加直し

--- fukuvHensu/Form1.cs
+++ fukuvHensu/Form1.cs
@@ -12,6 +12,7 @@ namespace fukuvHensu
 {
     public partial class Form1 : Form
     {
+        int vx = 0, vy = 0;
         public Form1()
         {
             InitializeComponent();
@@ -19,32 +20,32 @@ namespace fukuvHensu
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left += int.Parse(textBox1.Text);
-            label1.Top += int.Parse(textBox2.Text);
+            label1.Left += vx;
+            label1.Top +=vy;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "-5";
+            vx = 0;
+            vy = -5;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "-5";
-            textBox2.Text = "0";
+            vx = -5;
+            vy = 0;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "5";
-            textBox2.Text = "0";
+            vx = 5;
+            vy = 0;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "5";
+            vx = 0;
+            vy = 5;
         }
 
         private void label1_Click(object sender, EventArgs e)
